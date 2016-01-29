9d69e3d965de8435e5b8af0fe556c4ed8151ed35
Alexander Smith <alex@mapzen.com>
2016-01-29 11:51:14 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,173 @@
+/*
+  Modified from:
+  https://github.com/mynameistechno/finderjs/blob/master/example/example-async.js
+*/
+var layerOsm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  subdomains: ["a", "b", "c"],
+  maxZoom: 18
+});
+var map = new L.Map('map', {
+  measureControl: true
+}).addLayer(layerOsm).setView(new L.LatLng(37.7, -122.4), 6);
+var rspLayer = L.layerGroup();
+rspLayer.addTo(map);
+var stopLayer = L.layerGroup();
+stopLayer.addTo(map);
+
+var host = 'http://dev.transit.land'
+var container = document.getElementById('finder');
+var emitter = finder(container, remoteSource, {});
+
+function remoteSource(parent, cfg, callback) {
+  //var loadingIndicator = createLoadingColumn();
+  //cfg.emitter.emit('create-column', loadingIndicator);
+
+  stopLayer.clearLayers();
+  if (!parent || parent.type !== 'stop') rspLayer.clearLayers();
+  if (parent) {
+    if (parent.type === 'operator') {
+      loadRoutes(parent, cfg, callback);
+    }
+    else if (parent.type === 'route') {
+      loadRouteStopPatterns(parent, cfg, callback);
+    }
+    else if (parent.type === 'rsp') {
+      loadStops(parent, cfg, callback);
+    }
+    else if (parent.type === 'stop') {
+      displayStop(parent.rsp, parent.label, parent.index);
+    } else {}
+  }
+  else {
+    loadOperators(parent, cfg, callback);
+  }
+
+}
+
+function loadOperators(parent, cfg, callback) {
+  $.ajax({
+    url: host + '/api/v1/operators.json',
+    dataType: 'json',
+    async: true,
+    success: function(data) {
+      var finder_data = $.map(data.operators, function(operator) {
+        return {
+          label: operator.onestop_id,
+          type: 'operator'
+        }
+      });
+      callback(finder_data);
+    }
+  });
+}
+
+function loadRoutes(parent, cfg, callback) {
+  $.ajax({
+    url: host + '/api/v1/routes.json?operatedBy=' + parent.label,
+    dataType: 'json',
+    async: true,
+    success: function(data) {
+      var finder_data = $.map(data.routes, function(route) {
+        return {
+          label: route.onestop_id,
+          type: 'route',
+          route_stop_patterns: route.route_stop_patterns_by_onestop_id,
+        }
+      });
+      callback(finder_data);
+    }
+  });
+}
+
+function loadRouteStopPatterns(parent, cfg, callback) {
+  var finder_data = $.map(parent.route_stop_patterns, function(rsp_id) {
+    return {
+      label: rsp_id,
+      type: 'rsp'
+    }
+  });
+  callback(finder_data);
+}
+
+function loadStops(parent, cfg, callback) {
+  $.ajax({
+    url: host + '/api/v1/route_stop_patterns.geojson?onestop_id=' + parent.label,
+    dataType: 'json',
+    async: true,
+    success: function(data) {
+      stop_pattern = data.features[0].properties.stop_pattern;
+      var finder_data = $.map(stop_pattern, function(stop_onestop_id, i) {
+        var index = 0;
+        if (i == 0) index = -1;
+        else if (i == stop_pattern.length - 1) index = 1;
+        return {
+          label: stop_onestop_id,
+          type: 'stop',
+          rsp: parent.label,
+          index: index
+        }
+      });
+      displayRSP(data);
+      callback(finder_data);
+    }
+  });
+}
+
+function displayRSP(rsp_data) {
+  var geojson = L.geoJson(rsp_data, {
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(feature.id);
+    }
+  });
+  map.fitBounds(geojson.getBounds());
+  rspLayer.addLayer(geojson);
+}
+
+function getStop(stop_onestop_id, distance) {
+  $.ajax({
+    url: host + '/api/v1/stops.geojson?onestop_id=' + stop_onestop_id,
+    dataType: 'json',
+    async: true,
+    success: function(stop_data) {
+      var geojson = L.geoJson(stop_data, {
+        onEachFeature: function (feature, layer) {
+          layer.bindPopup(feature.id + '<br/>' +
+                          'Distance traveled: '+  distance);
+        }
+      });
+      console.log(geojson);
+      stopLayer.addLayer(geojson);
+      map.fitBounds(geojson.getBounds());
+    }
+  });
+}
+
+function displayStop(route_stop_pattern_onestop_id, stop_onestop_id, stop_index) {
+  var stop_query = stop_index === 1 ? '&destination_onestop_id=' : '&origin_onestop_id='
+  stop_query += stop_onestop_id
+  query = '/api/v1/schedule_stop_pairs.json?route_stop_pattern=' + route_stop_pattern_onestop_id
+  query += stop_query;
+  var distance;
+  $.ajax({
+    url: host + query,
+    dataType: 'json',
+    async: true,
+    success: function(data) {
+      if (stop_index === 1) distance = data.schedule_stop_pairs[0].destination_dist_traveled;
+      else distance = data.schedule_stop_pairs[0].origin_dist_traveled;
+      getStop(stop_onestop_id, distance);
+    }
+  })
+}
+
+function createLoadingColumn() {
+  var div = _.el('div.fjs-col.leaf-col');
+  var row = _.el('div.leaf-row');
+  var text = _.text('Loading...');
+  var i = _.el('span');
+
+  _.addClass(i, ['fa', 'fa-refresh', 'fa-spin']);
+  _.append(row, [i, text]);
+
+  return _.append(div, row);
+}
