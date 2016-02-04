19072fea8bf6c535ed3525f572e57dc9705730ab
Alexander Smith <alex@mapzen.com>
2016-02-04 09:48:26 -0800

    cleaning repository and minification

--- node_modules/finderjs/example/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var exampleStatic = require('./example-static');
-var exampleAsync = require('./example-async');
-
-
-exampleStatic(document.getElementById('container1'));
-exampleAsync(document.getElementById('container2'));
